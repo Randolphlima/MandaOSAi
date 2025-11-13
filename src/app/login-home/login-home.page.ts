07706ab2d1f0f6b906bbd06232ce8fc7f109ddd4
Randolph Lima <randolphrrl@gmail.com>
2025-11-13 17:38:37 -0300

    Configuração da rota do login para recuperação de senha

--- src/app/login-home/login-home.page.ts
+++ src/app/login-home/login-home.page.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonImg, IonList, IonItem, IonInput, IonButton  } from '@ionic/angular/standalone';
+import { RouterModule } from '@angular/router';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonImg, IonList, IonItem, IonInput, IonButton, IonFooter  } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-login-home',
   templateUrl: './login-home.page.html',
   styleUrls: ['./login-home.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonImg, IonList, IonItem, IonInput, IonButton]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonImg, IonList, IonItem, IonInput, IonButton, IonFooter, RouterModule]
 })
 export class LoginHomePage implements OnInit {
 
--- src/app/login-recovery/login-recovery.page.ts
+++ src/app/login-recovery/login-recovery.page.ts
@@ -1,18 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { NavController } from '@ionic/angular'; // Importe NavController
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton, IonIcon } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-login-recovery',
   templateUrl: './login-recovery.page.html',
   styleUrls: ['./login-recovery.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonBackButton, IonIcon]
 })
 export class LoginRecoveryPage implements OnInit {
 
-  constructor() { }
+  constructor(private navCtrl: NavController) {} // Injete NavController
+
+  goBack() {
+    // Retorna para a página anterior no histórico de navegação
+    this.navCtrl.back();
+    
+    // Alternativa: Se você sabe o caminho exato para onde voltar:
+    // this.navCtrl.navigateBack('/login-home'); 
+  }
 
   ngOnInit() {
   }
